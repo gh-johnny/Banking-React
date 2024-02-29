f3ac02f596fcdc95fc8987cb5642bfa9b0d86cfb
gh-johnny <jmfurtadoromero@gmail.com>
2024-02-29 10:28:48 -0300

    style: header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
+import { HeaderContainer, HeaderContent, LogoContainer, NewTransactionButton } from "./styles";
+
+import logoImg from '../../assets/logo/logo-johnny.svg'
+
 export function Header() {
     return (
-        <>
-            header
-        </>
+        <HeaderContainer>
+            <HeaderContent>
+                <LogoContainer>
+                    <img
+                        src={logoImg}
+                        width={130}
+                        height={130}
+                        alt="" // Screen reader will not read when alt tag is empty
+                    />
+                    <span>Banking</span>
+                </LogoContainer>
+
+                <NewTransactionButton
+                    type="button"
+                >
+                    New transaction
+                </NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
     )
 }
