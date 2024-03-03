5386672b7ecf9e447fdaf3448b3c94b17fa07286
gh-johnny <jmfurtadoromero@gmail.com>
2024-03-03 00:14:28 -0300

    chore: react-hook-form hookform/resolver zod for searchForm

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,16 +1,30 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import { searchFormSchema, TSearchFormSchema } from "../../../../libs/searchFormZodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export function SearchForm() {
+
+    const { register, handleSubmit, formState: { isSubmitting } } =
+        useForm<TSearchFormSchema>({ resolver: zodResolver(searchFormSchema) })
+
+    const handleSearchTransactions = async (data: TSearchFormSchema) => {
+        await new Promise(resolve => setTimeout(resolve, 4000))
+        console.log(data)
+    }
+
     return (
         <>
-            <SearchFormContainer>
+            <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
                 <input
                     type="text"
                     placeholder="Search for transactions"
+                    {...register('query')}
                 />
                 <button
-                    type="button"
+                    type="submit"
+                    disabled={isSubmitting}
                 >
                     <MagnifyingGlass size={20} />
                     Search
--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -31,7 +31,12 @@ export const SearchFormContainer = styled.form`
         border-radius: 6px;
         cursor: pointer;
 
-        &:hover {
+        &:disabled {
+            opacity: 0.7;
+            cursor: not-allowed;
+        }
+
+        &:not(:disabled):hover {
             background: ${props => props.theme["green-500"]};
             border: 1px solid ${props => props.theme["green-500"]};
             color: ${props => props.theme.white};
